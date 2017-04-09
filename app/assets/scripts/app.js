07c7f2ce9e39a4deb0ad7f4fdc03193cf4cfa4ef
Pradeep Talwar <fe.js18@gmail.com>
2017-04-09 04:49:26 -0400

    Completed Sticky Header functionality.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,8 @@
 import $ from 'jquery';
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
-
+import StickyHeader from "./modules/StickyHeader";
 var mobileMenu = new MobileMenu(); 
 new RevealOnScroll($('.icon'),'85%');
 new RevealOnScroll($('.btn'),'60%');
+var stickyHeader = new StickyHeader();
--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -4,17 +4,20 @@ class MobileMenu {
         this.siteHeader = $(".site-header");  
        this.menuIcon = $('.site-header__menu-icon'); 
         this.menuContent = $('.site-header__menu-content');
+        this.headerLinks = $(".primary-nav a");
         this.events();
         
     } 
     
     events() {
         this.menuIcon.click(this.toggleTheMenu.bind(this));
+        this.headerLinks.click(this.toggleTheMenu.bind(this));
     }
     
     toggleTheMenu() {
         this.menuContent.toggleClass("site-header__menu-content--is-visible");
         this.siteHeader.toggleClass("site-header--is-expanded");
+        
     } 
 }
 
