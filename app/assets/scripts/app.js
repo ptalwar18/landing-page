a64a7ef73fdd24287fbf33161040c6fb173db176
Pradeep Talwar <fe.js18@gmail.com>
2017-04-09 01:50:21 -0400

    Completed the reveal on scroll functionality

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,4 +1,7 @@
-var $ = require('jquery');
+import $ from 'jquery';
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
 
 var mobileMenu = new MobileMenu(); 
+new RevealOnScroll($('.icon'),'85%');
+new RevealOnScroll($('.btn'),'60%');
